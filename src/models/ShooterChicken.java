34dcdfe156420463013b308f35ec165a401d6afd
Melina Masoumi <melina.masoumi@gmail.com>
2026-07-05 17:45:04 +0330

    add enemy types

--- src/models/ShooterChicken.java
+++ src/models/ShooterChicken.java
@@ -1,4 +1,16 @@
 package models;
 
-public class ShooterChicken {
+public abstract class ShooterChicken extends Chicken {
+
+    public ShooterChicken(int x, int y) {
+
+        super(x, y, 50, 50, 2, 2);
+
+    }
+
+    @Override
+    public void move(int direction) {
+
+
+    }
 }
--- src/models/ZigzagChicken.java
+++ src/models/ZigzagChicken.java
@@ -1,4 +1,21 @@
 package models;
 
-public class ZigzagChicken {
+public class ZigzagChicken extends Chicken {
+
+    private boolean movingDown = true;
+
+    public ZigzagChicken(int x, int y) {
+
+        super(x, y, 50, 50, 2, 2);
+
+    }
+
+    @Override
+    public void move(int direction) {
+
+    }
+    @Override
+    public int getScore(){
+        return 20;
+    }
 }
